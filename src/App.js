a504bf3a50bb69119880eba1b2b122d1eecf68f9
DanDecoito <116022076+DanDecoito@users.noreply.github.com>
2023-03-17 12:08:50 -0700

    Info

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// Daniel Decoito
+// 3-17-23
+// Recreate a social media dashboard to specifications, post on github and azure
+// Reviews: 
+
+
+
 import './App.css';
 import MainComponent from './components/mainComponent/mainComponent';
 
